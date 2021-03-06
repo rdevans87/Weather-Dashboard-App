4423695f8a16d9976dda850b5313eea851463fb6
Ryan Evans <rdevans87@gmail.com>
2021-03-06 04:02:27 -0500

    working thru js search query param and searchApi function

--- assets/js/display-search.js
+++ assets/js/display-search.js
@@ -1,14 +1,30 @@
 var resultTextEl = document.querySelector('#result-text');
 var resultContentEl = document.querySelector('#result-content');
 var searchFormEl = document.querySelector('#search-form');
+var responseText = document.getElementById('response-text');
 
 function getParams() {
 
-    var searchParamsArr = document.location.search.split('&');
+searchApi(query, format);
 
-  // Get the query and format values
-  var query = searchParamsArr[0].split('=').pop();
-  var format = searchParamsArr[1].split('=').pop();
-
-  searchApi(query, format);
 }
+    
+    
+
+var requestUrl = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid={7c311763d4d62a265ed239e3d4ae2f45}"
+
+getApi(requestUrl) {
+  fetch(requestUrl)
+    .then(function (response) {
+      console.log(response);
+      if (response.status === 200) {
+        responseText.textContent = response.status;
+      }
+      return response.json();
+  });
+}
+
+getApi(requestUrl);
+
+
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,19 @@
-var searchFormEl = document.querySelector('#search-form');
+var searchFormEl = document.querySelector('#weather-search-form');
 
 function handleSearchFormSubmit(event) {
 event.preventDefault();
-
-
 
 var searchInputVal = document.querySelector('#search-input').value;
 var formatInputVal = document.querySelector("#format-input").value;
 
 if (!searchInputVal) {
-console.error('please enter a ciy ')
-
+console.error('please enter a ciy');
+return;
 }
 
 var queryString = './search-results.html?q=' + searchInputVal + '&format=' + formatInputVal;
 
-  location.assign(queryString);
-
+  ocation.assign(queryString);
 }
 
 
