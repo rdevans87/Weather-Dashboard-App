cae8c856a77055582aa1a998e5396109f71a1d18
Ryan Evans <rdevans87@gmail.com>
2021-03-06 21:13:14 -0500

    restructuring html and css

--- assets/js/display-search.js
+++ assets/js/display-search.js
@@ -5,14 +5,31 @@ var responseText = document.getElementById('response-text');
 
 function getParams() {
 
-var searchParamsArr = document.location.search.split('&');
+var searchParamsArr = document.location.search.split('&')
 
-searchApi(query, format);
+searchApi(query, city);
 
 var query = searchParamsArr[0].split('=').pop();
-var format = searchParamsArr[1].split('=').pop()
+var city = searchParamsArr[1].split('=').pop()
 
 }
+
+function printResults(resultObj) {
+    console.log(resultObj);
+
+    var resultCard = document.createElement('div');
+  resultCard.classList.add('card', 'bg-light', 'text-dark', 'mb-3', 'p-3');
+
+  var resultBody = document.createElement('div');
+  resultBody.classList.add('card-body');
+  resultCard.append(resultBody);
+
+  var titleEl = document.createElement('h3');
+  titleEl.textContent = resultObj.title;
+
+  var bodyContentEl = document.createElement('p');
+  bodyContentEl.innerHTML =
+    '<strong>Date:</strong> ' + resultObj.date + '<br/>';
     
     
 
