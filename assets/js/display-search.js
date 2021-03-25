61d40e92ce1bdecb55b8b06b94b007955542af2f
Ryan Evans <rdevans87@gmail.com>
2021-03-25 03:16:52 -0400

    data returns as undefined

--- assets/js/display-search.js
+++ assets/js/display-search.js
@@ -7,7 +7,8 @@ var resultTextEl = document.querySelector("#result-text");
 
 
 function getWeatherOnSubmit(event) {
-  event.preventDefault()
+  event.preventDefault();
+
   var cityInputVal = document.querySelector('#city-input').value;
 
   var openWeatherApi = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityInputVal + "&appid=" + "74a15e51334b346d7b36d2511a654776";
@@ -15,21 +16,37 @@ function getWeatherOnSubmit(event) {
   openWeatherApi = openWeatherApi + '&q=' + cityInputVal;
   fetch(openWeatherApi)  
     .then(function (response) {
-      if (!response.ok) {
-        throw response.json();
-      }
-      return response.json();
-   })
-     .then(function (data) {
-      resultTextEl.textContent = openWeatherApi.cityInputVal;
-      displayWeatherResults(data);
-     });
+        console.log(response)
+        return response.json();
+    })
+     .then(function (data) { 
+       console.log(data)
+
+        var resultTextEl = document.querySelector('#result-content');
+        resultTextEl.innerHTML = "<h2>City: " + data + "</h2>";
+        cityName.textContent = "text content";
+        resultTextEl.textContent = data.weather;
+        resultContentEl.append(cityName);
+        resultContentEl.append(weather);
+        
+    });
 }
        
-      function displayWeatherResults(data) {
-        console.log(data)
-        
-        resultTextEl.innerHTML =  ('<h2>City:</h2>');
+  //     function displayWeather(data) {
+  //       var temp = document.querySelector("#result-list");
+  //       temp.textContent = "This is the temp element"
+  //       resultListEl.append(temp);
+  //       resultTextEl = document.querySelector(".card card-rounded");
+  //       resultTextEl.textContent = "<h2>City: " + (data.name) + "</h2>";
+  //       resultTextEl.append(temp);
+    
+  // }     
+      // var cityName = object[i].name;
+      // var conditionsList = object[i].list.wind.speed
+
+      // cityName = document.append('h2');
+      // conditionsList = document.append('li');
+       
       
-}
+
 searchFormEl.addEventListener('submit', getWeatherOnSubmit);
