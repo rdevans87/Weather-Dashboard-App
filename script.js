8151023ac0835b8d9962f8ea15c9558769963a04
Ryan Evans <rdevans87@gmail.com>
2021-03-28 07:58:58 -0400

    adding apionecall for uvindex and current weather

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 var resultContent = document.querySelector('#result-content');
 var searchFormEl = document.querySelector('#search-form');
-var savedSearchEl = document.getElementById("#list-group-item");
-var citySearchList = document.getElementById("#saved-search-list");
 var resultListEl = document.getElementById("#result-list");
 var resultTextEl = document.querySelector("#result-text");
-var backButtonEl = document.getElementById("back-button");
-
+var backButtonEl = document.getElementById("#back-button");
+var forecastResults = document.querySelector("#forecast-results")
 
 function getWeatherOnSubmit(event) {
   event.preventDefault();
+
+  for (var i = 0; i < localStorage.length; i++) {
+    citySearchList = cityInputVal.localStorage.getItem(i);
+    citySearchList.append("<button class='list-group-item'>" + cityInputVal + "</button>");
+    citySearchList.innerHTML = "<button class='list-group-item'>" + cityInputVal + "</button>";
+  }
+
+    
   
   var cityInputVal = document.querySelector('#city-input').value;
 
@@ -27,12 +33,31 @@ function getWeatherOnSubmit(event) {
         var humidity = data.list[0].main.humidity;
         var temp = data.list[0].main.temp;
         var wind =  data.list[0].wind.speed;
-        console.log(temp, humidity, wind)
+        // var icon  = data.list[0].weather.main.icon;
+        console.log(temp, humidity, wind);
         resultContent = document.querySelector('#result-content');
         resultContent.innerHTML = "<h2>City: " + cityInputVal.toUpperCase(title) + "</h2>" + "<br>" + "<li>Temperature: " + 
-        temp + "&#8457" + "</li>" + "<br>" + "<li>Humidity: " + humidity + "</li>" + "<br>" + "<li>Wind Speed: " + wind + "</li>";
-    })
+        temp + "&#8457" + "</li>" + "<br>" + "<li>Humidity: " + humidity + "%" + "</li>" + "<br>" + "<li>Wind Speed: " + wind + " MPH" + "</li>";
+          
+
+        var apiOneCall = "https://api.openweathermap.org/data/2.5/onecall?" + "lat=" + data.city.coord.lat + "&lon=" + data.city.coord.lon + "&units=imperial" + "&appid=" + "b0ff6d197a40a5f8e42c9a3871298d52";
+        
+        fetch(apiOneCall)
+        .then(function (response) {
+          console.log(response)
+          return response.json();
+        })
+         .then(function (data) { 
+           console.log(data)
+           coord = apiOneCall;
+      })
+   
+      });
+
+
+  
 }
+
 
 
 
