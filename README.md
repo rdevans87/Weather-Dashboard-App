3414af1b2d61772b8bfc1a3e3757f6dd4074b394
Ryan Evans <rdevans87@gmail.com>
2021-04-19 21:42:54 -0400

    was able to append to page for city search list

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
  # 06 Server-Side APIs: Weather Dashboard
 
-## Coder Story
+## Description
 
-This application features a weather dashboard that runs in the browser and contains a variety of features and robust search capabilities, displaying precise data and up to date weather conditions for over 200,000 cities across the United States. Bootstrap was used to build the primary front-end frame work with additional HTML and CSS properties added for a more personalized style and responsive design. This app relies heavily on Javascript methods and principles to provide the essential functionality outlined in the acceptance criteria.  
+This application features a weather dashboard that runs in the browser and contains a variety of features and robust search capabilities, displaying precise data and up to date weather conditions for over 200,000 cities across the United States. Bootstrap was used to build the primary front-end framework with additional HTML and CSS properties added for a more personalized style and responsive design. This app relies heavily on Javascript methods and principles to provide the essential functionality outlined in the acceptance criteria.  
 
 In addition, this app required data requests from two different server-side APIs in order to obtain the accurate weather conditions and icon representations needed for both current and future conditions. [OpenWeather API](https://openweathermap.org/api) is a team of IT experts and data scientists that has been practiscing deep weather data science since 2014. For each point on the globe, OpenWeather provides historical, current and forecasted weather data via light-speed APIs.
 
@@ -10,16 +10,21 @@ APIs USED:
 
 [Current Weather Data](https://openweathermap.org/current) 
 
+City, Date, Icon-image
+Temperature
+Humidity
+Wind Speed
+UV index
+
 [One Call](https://openweathermap.org/api/one-call-api) 
 
+Date
+Icon image
+Temperature
+Humidity
 
-The Current Weather Data API was used as the primary search parameter "By City Name". The user can type any city name and hit "submit" to see the current weather including the specific condtions temperature, humidity, wind speed. The One Call API was used to retriece the UVindex along with the 5 days forecast.  `localStorage` to store specific parameters of my search, which are listed below the search bar.  I used a fetch call to gather the search criteria with an API key provided by the site and stored the data in local storage.  
+The Current Weather Data API was used as the primary search parameter "By City Name". The user can type any city name and hit "submit" to see the current weather including the specific conditions temperature, humidity, wind speed. The One Call API was used to retrieve the UVindex along with the 5 day forecast.  `localStorage` is used store specific parameters of the search, which are listed below the search bar. 
 
-
-
-
-
-======
 
 ## User Story
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -5,15 +5,16 @@ var resultTextEl = document.querySelector("#result-text");
 var backButtonEl = document.getElementById("#back-button");
 var forecastResults = document.querySelector("#forecast-results");
 var weatherIcon = document.querySelector("#weather-icon");
-var citySearch = [];
-var cities = [];
+var citySearch  = []
+var cities = []
 
 
-  function getWeatherOnSubmit(event) {
+function getWeatherOnSubmit(event) {
   event.preventDefault();
   
   var cityInputVal = document.querySelector('#city-input').value;
  
+
   //connnect to open weather API five day forecast. cityInput as value.
   var openWeatherApi = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityInputVal + "&units=imperial" + "&appid=" + "b0ff6d197a40a5f8e42c9a3871298d52";
 
@@ -26,57 +27,63 @@ var cities = [];
     })
     .then(function (data) {
       console.log(data)
-      var city = cityInputVal;
-      console.log(city);
-      window.localStorage.setItem(city , JSON.stringify(city));
-      for (var i = 0; i < city.length; i++) {
-      // window.localStorage.getItem(city)
-      document.getElementById("city1").innerHTML = city;
-        if (city === i) {
-        
-        }
+      var city = cityInputVal
+      console.log(city)
+      window.localStorage.setItem("city", city)
+      var cities = document.querySelector("#city-search-list")
+      cities.innerHTML = "<button class=" + 'list-group-item' + "id='city1'>" + city + "</button>";
+      cities = city++;
+           
+         
+      // cityInputVal = document.querySelector('#city-input').value;
+      
+      // cityInputVal = document.querySelector("#city-search-list")
+      // var cities = city.value
+      // var citySearch = document.createElement("button").classList.add(".list-group-item")
+      // citySearch.innerHTML = "<button>" + city + "</button>";
+      // cities = city++;
+     
+        // var cities = city++; 
+         // document.querySelector(".list-group-item").append(city)
+      
+
+     
+
+      //use cityInput coordinates from forecast to apply Onecall Api for current weather and UV index. UV Index API deprecated on 04/01/2020.
+      var apiOneCall = "https://api.openweathermap.org/data/2.5/onecall?" + "lat=" + data.city.coord.lat + "&lon=" + data.city.coord.lon + "&units=imperial" + "&appid=" + "b0ff6d197a40a5f8e42c9a3871298d52";
 
 
-
-    } 
-    
-    
-         //use cityInput coordinates from forecast to apply Onecall Api for current weather and UV index. UV Index API deprecated on 04/01/2020.
-       var apiOneCall = "https://api.openweathermap.org/data/2.5/onecall?" + "lat=" + data.city.coord.lat + "&lon=" + data.city.coord.lon + "&units=imperial" + "&appid=" + "b0ff6d197a40a5f8e42c9a3871298d52";
-          
-       
-        fetch(apiOneCall)
+      fetch(apiOneCall)
         .then(function (response) {
-        console.log(response)
-        return response.json();
-       })
+          console.log(response)
+          return response.json();
+        })
         .then(function (data) {
-         console.log(data);
-           var humidity = data.current.humidity;
-           var uvi = data.current.uvi;
-          //  uvi = querySelector('#back-ground')
-          //  uvi.setAttribute("background-color", "red")
-           var temp = data.current.temp;
+          console.log(data);
+
+          var humidity = data.current.humidity;
+          var uvi = data.current.uvi;
+          var temp = data.current.temp;
           var wind_speed = data.current.wind_speed;
           var currentWeather = data.current.weather[0].description;
           weatherIcon = data.current.weather[0].icon;
-          var currentDay =" (" + moment(data.current.dt.value).format("MMM D, YYYY") + ") ";
+          var currentDay = " (" + moment(data.current.dt.value).format("MMM D, YYYY") + ") ";
           console.log(uvi, temp, humidity, wind_speed, currentWeather, weatherIcon, currentDay);
-         resultContent = document.querySelector('#result-content');
-         resultContent.innerHTML = "<h2>" + cityInputVal.toUpperCase(city) + currentDay + "</h2>" + "<div>" + "<img src=" + "https://openweathermap.org/img/wn/" + weatherIcon + "@2x.png>" + "</div>" + "<br>" + "<p>Forecast: " + "<strong>" + currentWeather + "</strong>" + "</p>" + "<li>Temperature: " + "<strong>" +
-          temp + "&#8457" + "</strong>" + "</li>" + "<br>" + "<li>Humidity: " + "<strong>" + humidity + "%" + "</strong>" + "</li>" + "<br>" + "<li>Wind Speed: " + "<strong>" + wind_speed + " MPH" + "</strong>" + "</li>" + "<br>" + "<li>UV Index: " + "<strong>" + uvi + "</strong>" + "</li>";
-          
+          resultContent = document.querySelector('#result-content');
+          resultContent.innerHTML = "<h2>" + cityInputVal.toUpperCase(city) + currentDay + "</h2>" + "<div>" + "<img src=" + "https://openweathermap.org/img/wn/" + weatherIcon + "@2x.png>" + "</div>" + "<br>" + "<p>Forecast: " + "<strong>" + currentWeather + "</strong>" + "</p>" + "<li>Temperature: " + "<strong>" +
+            temp + "&#8457" + "</strong>" + "</li>" + "<br>" + "<li>Humidity: " + "<strong>" + humidity + "%" + "</strong>" + "</li>" + "<br>" + "<li>Wind Speed: " + "<strong>" + wind_speed + " MPH" + "</strong>" + "</li>" + "<br>" + "<li>UV Index: " + "<strong>" + uvi + "</strong>" + "</li>";
+
           //append data for 5 day forecast
-          dailyWeather = data.daily[1]; 
+          dailyWeather = data.daily[1];
           tempOne = data.daily[1].temp.day;
-          humidityOne= data.daily[1].humidity
-          uviOne = data.daily[1].uvi 
+          humidityOne = data.daily[1].humidity
+          uviOne = data.daily[1].uvi
           weatherOne = data.daily[1].weather[0].icon
           var dailyOne = moment.unix(data.daily[1].dt).format("MMM D, YYYY");
           console.log(humidityOne, tempOne, uviOne, weatherOne, dailyOne)
           forecastResults = document.querySelector('#dayOne');
-          forecastResults.innerHTML = "<h3>" + dailyOne + "</h3>" + "<div>" + "<img src=" + "https://openweathermap.org/img/wn/" + weatherOne + "@2x.png>" + "</div>" + "<p>Temp: " + tempOne + "&#8457" + "</p>" + "<p>Humidity: " + humidityOne + "%" + "</p>" +  "<p>UV Index: " + uviOne + "</p>" ;                                 
-        
+          forecastResults.innerHTML = "<h3>" + dailyOne + "</h3>" + "<div>" + "<img src=" + "https://openweathermap.org/img/wn/" + weatherOne + "@2x.png>" + "</div>" + "<p>Temp: " + tempOne + "&#8457" + "</p>" + "<p>Humidity: " + humidityOne + "%" + "</p>" + "<p>UV Index: " + uviOne + "</p>";
+
           forecastTwo = data.daily[2];
           tempTwo = data.daily[2].temp.day;
           humidityTwo = data.daily[2].humidity;
@@ -85,8 +92,8 @@ var cities = [];
           var dailyTwo = moment.unix(data.daily[2].dt).format("MMM D, YYYY");
           console.log(humidityTwo, tempTwo, uviTwo, dailyTwo)
           forecastResults = document.querySelector('#daytwo');
-          dayTwo.innerHTML = "<h3>" + dailyTwo + "</h3>" + "<div>" + "<img src=" + "https://openweathermap.org/img/wn/" + weatherTwo + "@2x.png>" + "</div>" + "<p>Temp: " + tempTwo + "&#8457" + "</p>" + "<p>Humidity: " + humidityTwo + "%" + "</p>" + "<p>UV Index: " + uviTwo + "</p>" ;                                 
-        
+          dayTwo.innerHTML = "<h3>" + dailyTwo + "</h3>" + "<div>" + "<img src=" + "https://openweathermap.org/img/wn/" + weatherTwo + "@2x.png>" + "</div>" + "<p>Temp: " + tempTwo + "&#8457" + "</p>" + "<p>Humidity: " + humidityTwo + "%" + "</p>" + "<p>UV Index: " + uviTwo + "</p>";
+
           forecastThree = data.daily[3];
           tempThree = data.daily[3].temp.day;
           humidityThree = data.daily[3].humidity;
@@ -95,41 +102,42 @@ var cities = [];
           var dailyThree = moment.unix(data.daily[3].dt).format("MMM D, YYYY");
           console.log(humidityThree, tempThree, uviThree, dailyThree);
           forecastResults = document.querySelector('#dayThree');
-          forecastResults.innerHTML = "<h3>" + dailyThree + "</h3>" + "<div>" + "<img src=" + "https://openweathermap.org/img/wn/" + weatherThree + "@2x.png>" + "</div>"  + "<p>Temp: " + tempThree + "&#8457" + "</p>" + "<p>Humidity: " + humidityThree + "%" + "</p>" + "<p>UV Index: " + uviThree + "</p>" ;                                 
-        
+          forecastResults.innerHTML = "<h3>" + dailyThree + "</h3>" + "<div>" + "<img src=" + "https://openweathermap.org/img/wn/" + weatherThree + "@2x.png>" + "</div>" + "<p>Temp: " + tempThree + "&#8457" + "</p>" + "<p>Humidity: " + humidityThree + "%" + "</p>" + "<p>UV Index: " + uviThree + "</p>";
+
           forecastFour = data.daily[4];
           tempFour = data.daily[4].temp.day;
           humidityFour = data.daily[4].humidity;
-          uviFour = data.daily[4].uvi; 
+          uviFour = data.daily[4].uvi;
           weatherFour = data.daily[4].weather[0].icon
           var dailyFour = moment.unix(data.daily[4].dt).format("MMM D, YYYY");
           console.log(humidityFour, tempFour, uviFour, dailyFour);
           forecastResults = document.querySelector('#dayFour');
-          forecastResults.innerHTML = "<h3>" + dailyFour + "</h3>" + "<div>" + "<img src=" + "https://openweathermap.org/img/wn/" + weatherFour + "@2x.png>" + "</div>" + "<p>Temp: " + tempFour + "&#8457" + "</p>" + "<p>Humidity: " + humidityFour + "%" + "</p>" + "<p>UV Index: " + uviFour + "</p>" ;
+          forecastResults.innerHTML = "<h3>" + dailyFour + "</h3>" + "<div>" + "<img src=" + "https://openweathermap.org/img/wn/" + weatherFour + "@2x.png>" + "</div>" + "<p>Temp: " + tempFour + "&#8457" + "</p>" + "<p>Humidity: " + humidityFour + "%" + "</p>" + "<p>UV Index: " + uviFour + "</p>";
 
           forecastFive = data.daily[5];
           tempFive = data.daily[5].temp.day;
           humidityFive = data.daily[5].humidity;
-          uviFive = data.daily[5].uvi; 
+          uviFive = data.daily[5].uvi;
           weatherFive = data.daily[5].weather[0].icon
           var dailyFive = moment.unix(data.daily[5].dt).format("MMM D, YYYY");
-          console.log (humidityFive, tempFive, uviFive, dailyFive);
+          console.log(humidityFive, tempFive, uviFive, dailyFive);
           forecastResults = document.querySelector('#dayFive');
-          forecastResults.innerHTML =  "<h3>" + dailyFive + "</h3>" + "<div>" + "<img src=" + "https://openweathermap.org/img/wn/" + weatherFive + "@2x.png>" + "</div>" + "<p>Temp: " + tempFive + "&#8457" + "</p>" + "<p>Humidity: " + humidityFive + "%" + "</p>" + "<p>UV Index: " + uviFive + "</p>";
-            
+          forecastResults.innerHTML = "<h3>" + dailyFive + "</h3>" + "<div>" + "<img src=" + "https://openweathermap.org/img/wn/" + weatherFive + "@2x.png>" + "</div>" + "<p>Temp: " + tempFive + "&#8457" + "</p>" + "<p>Humidity: " + humidityFive + "%" + "</p>" + "<p>UV Index: " + uviFive + "</p>";
+
+         
+
+
         });
-            
+
        
         
-
-
-
+      
+    
     });
+      
+      
+      
 
-   
-    
-    
-              
 }
 
 searchFormEl.addEventListener('submit', getWeatherOnSubmit);
